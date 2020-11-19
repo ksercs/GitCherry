2b2d4042d905ce43105160179e67cdb7898abff5
Jaan Toming <jtoming@gmail.com>
2020-11-19 13:23:37 +0300

    tiny changes

--- dxcherry/src/extension.ts
+++ dxcherry/src/extension.ts
@@ -6,76 +6,95 @@ export function activate(context: vscode.ExtensionContext) {
   vscode.window.registerTreeDataProvider('exampleView', new TreeDataProvider());
 }
 
-class TreeDataProvider implements vscode.TreeDataProvider<extendedTreeItem> {
-  data: extendedTreeItem[];
+class TreeDataProvider implements vscode.TreeDataProvider<ExtendedTreeItem> {
+  data: ExtendedTreeItem[];
 
   constructor() {
-    this.data = [new extendedTreeItem('Pull request settings', [
+    this.data = [new ExtendedTreeItem('Pull request settings', [
       this.getReviewersTree(),
       this.getLabelsTree()
     ])];
 
 		vscode.commands.registerCommand('treeView.selectTreeItem', (element) => this.onItemClicked(element));
+		vscode.commands.registerCommand('start', () => this.onStart());
   }
 
-  getReviewersTree(): extendedTreeItem {
-    return new extendedTreeItem('Reviewers', [
-      new extendedTreeItem('ksercs'),
-      new extendedTreeItem('jtoming830')
+  onStart() {
+    const titleInput = vscode.window.createInputBox();
+    const values = [];
+    titleInput.title = "Enter pull request's title";
+    titleInput.totalSteps = 2;
+    titleInput.step = 1;
+    titleInput.onDidAccept(() => {
+      values.push(titleInput.value);
+      if(titleInput.step === 1) {
+        titleInput.value = "";
+        titleInput.step = 2;
+        titleInput.title = "Enter pull request's description";
+      } else {
+        titleInput.dispose();
+        vscode.window.showInformationMessage(values.join(' _ '));
+        // pass values to the next step
+      }
+    });
+    titleInput.show();
+  }
+
+  getReviewersTree(): ExtendedTreeItem {
+    return new ExtendedTreeItem('Reviewers', [
+      new ExtendedTreeItem('ksercs'),
+      new ExtendedTreeItem('jtoming830')
     ]);
   }
 
-  getLabelsTree(): extendedTreeItem {
-    return new extendedTreeItem('Labels', [
-      new extendedTreeItem('bug'),
-      new extendedTreeItem('wontfix'),
-      new extendedTreeItem('invalid'),
-      new extendedTreeItem('question'),
-      new extendedTreeItem('duplicate')
+  getLabelsTree(): ExtendedTreeItem {
+    return new ExtendedTreeItem('Labels', [
+      new ExtendedTreeItem('bug'),
+      new ExtendedTreeItem('wontfix'),
+      new ExtendedTreeItem('invalid'),
+      new ExtendedTreeItem('question'),
+      new ExtendedTreeItem('duplicate')
     ]);
   }
 
-  setIcon(element: extendedTreeItem) {
+  setIcon(element: ExtendedTreeItem) {
     if(element.children?.length) {
       return;
     }
 
     if(element.selected) {
       element.iconPath = {
-        light: path.join(__filename, '..', '..', 'resources', 'checked.svg'),
-        dark: path.join(__filename, '..', '..', 'resources', 'checked.svg')
-      }
+        light: path.join(__filename, '..', '..', 'resources', 'light', 'checked.svg'),
+        dark: path.join(__filename, '..', '..', 'resources', 'dark', 'checked.svg')
+      };
     } else {
       element.iconPath = {
-        light: path.join(__filename, '..', '..', 'resources', 'unchecked.svg'),
-        dark: path.join(__filename, '..', '..', 'resources', 'unchecked.svg')
-      }
+        light: path.join(__filename, '..', '..', 'resources', 'light', 'unchecked.svg'),
+        dark: path.join(__filename, '..', '..', 'resources', 'dark', 'unchecked.svg')
+      };
     }
   }
 
-  getTreeItem(element: extendedTreeItem): extendedTreeItem|Thenable<extendedTreeItem> {
+  getTreeItem(element: ExtendedTreeItem): ExtendedTreeItem|Thenable<ExtendedTreeItem> {
     this.setIcon(element);
     element.command = { command: 'treeView.selectTreeItem', title: "Select item", arguments: [element], };
     return element;
   }
 
-  private _onDidChangeTreeData: EventEmitter<
-    extendedTreeItem | null
-  > = new EventEmitter<extendedTreeItem | null>();
+  private _onDidChangeTreeData: EventEmitter<ExtendedTreeItem | null> = new EventEmitter<ExtendedTreeItem | null>();
 
-  readonly onDidChangeTreeData: Event<extendedTreeItem | null> = this
-    ._onDidChangeTreeData.event;
+  readonly onDidChangeTreeData: Event<ExtendedTreeItem | null> = this._onDidChangeTreeData.event;
 
-  refresh(element: extendedTreeItem | null): void {
+  refresh(element: ExtendedTreeItem | null): void {
     this._onDidChangeTreeData.fire(element);
   }
 
-  onItemClicked(element: extendedTreeItem) {
+  onItemClicked(element: ExtendedTreeItem) {
     element.selected = !element.selected;
     this.refresh(element);
   }
 
-  getChildren(element?: extendedTreeItem|undefined): vscode.ProviderResult<extendedTreeItem[]> {
+  getChildren(element?: ExtendedTreeItem|undefined): vscode.ProviderResult<ExtendedTreeItem[]> {
     if (element === undefined) {
       return this.data;
     }
@@ -83,11 +102,11 @@ class TreeDataProvider implements vscode.TreeDataProvider<extendedTreeItem> {
   }
 }
 
-class extendedTreeItem extends vscode.TreeItem {
-  children: extendedTreeItem[]|undefined;
+class ExtendedTreeItem extends vscode.TreeItem {
+  children: ExtendedTreeItem[]|undefined;
   selected: boolean;
 
-  constructor(label: string, children?: extendedTreeItem[]) {
+  constructor(label: string, children?: ExtendedTreeItem[]) {
     super(
         label,
         children === undefined ? vscode.TreeItemCollapsibleState.None :
